d66560bdd8c5bd153130d6d767f435a047bed253
SGKumar <ssgkumar.tech@gmail.com>
2018-06-24 17:54:00 +0530

    Misc changes

--- test/com/alprep/sample/AnotherSampleTest.java
+++ test/com/alprep/sample/AnotherSampleTest.java
@@ -6,11 +6,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class AnotherSampleTest {
-
     @Test public void anotherSample() {
-
         assertEquals("another Gradle is great", "another Gradle is great");
-
     }
-
 }
--- test/com/alprep/sample/TestSample.java
+++ test/com/alprep/sample/TestSample.java
@@ -5,12 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class TestSample {
-
     @Test public void sample() {
-
         assertEquals("sample Gradle is gr8", "sample Gradle is gr8");
-
     }
-
 }
 
