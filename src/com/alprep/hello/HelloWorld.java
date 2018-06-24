8f1af45a36d9c2e17b8396e2e4a9395d762f976f
SGKumar <ssgkumar.tech@gmail.com>
2018-06-24 20:10:56 +0530

    simple changes

--- src/com/alprep/hello/HelloWorld.java
+++ src/com/alprep/hello/HelloWorld.java
@@ -3,7 +3,6 @@ package com.alprep.hello;
 public class HelloWorld {
   public String getGreeting() {
     return new Greeter().sayHello();
-
   }
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
